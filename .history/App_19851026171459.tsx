a27ab3c4e249b62efcf1b85696c7ced55397ab1d
Shun Ueji <136816807+chinsukou@users.noreply.github.com>
2023-10-19 23:54:20 +0900

    Install ESLint

--- .history/App_19851026171459.tsx
+++ .history/App_19851026171459.tsx
@@ -1,13 +1,13 @@
-import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
-const App = (): JSX.Element => {
+export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
@@ -15,8 +15,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center'
-  }
-})
-
-export default App
+    justifyContent: 'center',
+  },
+});
--- .history/App_20231019234545.tsx
+++ .history/App_20231019234545.tsx
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, Text, View } from 'react-native'
 
-const App = (): JSX.Element => {
+export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
@@ -18,5 +18,3 @@ const styles = StyleSheet.create({
     justifyContent: 'center'
   }
 })
-
-export default App
