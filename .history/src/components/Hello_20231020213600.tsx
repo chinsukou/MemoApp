6c3c1da20bbe5fc51798e38c2fced7162bcf6789
Shun Ueji <136816807+chinsukou@users.noreply.github.com>
2023-10-21 16:58:04 +0900

    Create Hello Component

---
+++ .history/src/components/Hello_20231020213600.tsx
@@ -0,0 +1,26 @@
+import { View, Text, StyleSheet } from 'react-native'
+
+interface Props {
+  children: string
+}
+
+const Hello = (props: Props): JSX.Element => {
+  const { children} = props
+  return (
+    <View>
+      <Text style={styles.text}>Hello {children}</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  text: {
+    color: '#ffffff',
+    backgroundColor: 'blue',
+    fontSize: 40,
+    fontWeight: 'bold',
+    padding: 16
+  }
+})
+
+export default Hello
