ed180307ecbdb987154618e28bd0214a68057a0b
Shun Ueji <136816807+chinsukou@users.noreply.github.com>
2023-10-21 23:05:35 +0900

    Install expo-router

---
+++ .history/src/app/_layout_20231021230329.tsx
@@ -0,0 +1,5 @@
+import { Slot } from 'expo-router'
+
+const Layout = () => {
+  return <Slot />
+}
---
+++ .history/src/app/index_20231021225812.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native'
+
+const Index = () => {
+  return (
+    <View>
+      <Text>Hello</Text>
+    </View>
+  )
+}
